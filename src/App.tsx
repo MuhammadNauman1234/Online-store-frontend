86f37e937b4f96da197e18f629c56c9872ab3978
Muhammad Nauman <nkbangash03@gmail.com>
2025-08-22 19:48:10 +0500

    routing implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import Home from "./pages/Home"
+import AddItems from "./pages/AddItems"
+import Items from "./pages/Items"
+import Checkout from "./pages/Checkout"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import MainLayout from "./components/layouts/MainLayout"
 
 function App() {
 
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="add-item" element={<AddItems />} />
+          <Route path="items" element={<Items />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>          
   )
 }
 
--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,7 +1,11 @@
+import { Outlet } from "react-router-dom"
+import Header from "../common/Header"
+
 const MainLayout = () => {
   return (
     <div>
-      
+      <Header />
+      <Outlet />
     </div>
   )
 }
